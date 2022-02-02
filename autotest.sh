3c3a093f3f81bd1227f9bc6f32c12505098d3767
Walter B. Vaughan <wbv@users.noreply.github.com>
2022-02-02 12:07:54 -0500

    add comments to autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# run this in a terminal to instantly re-run the test suite anytime you change
+# anything in this project. if you work in an adjacent window, you'll instantly
+# see your results as soon as you hit "save"
+
+# NOTE: requires inotifywait (try `apt install inotify-tools` on ubuntu/debian)
+
 while :; do
 	inotifywait -e close_write,moved_to,create -rq . 
 	clear
