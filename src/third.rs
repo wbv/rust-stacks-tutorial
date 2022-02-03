8fdf7a09856ff6e9ff27b7b35ffd747997806baa
Walter B. Vaughan <wbv@users.noreply.github.com>
2022-02-03 18:06:39 -0500

    begin third implementation

---
+++ src/third.rs
@@ -0,0 +1,70 @@
+use std::rc::Rc;
+
+/// This is an immutable linked list, like in a functional programming language.
+pub struct List<T> {
+    head: Link<T>
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    /// Default, empty constructor for a [`List`]
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    /// Attach a new element `elem` to the front of a [`List`]
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            }))
+        } 
+    }
+
+    /// Return the tail of a [`List`] which is every element besides the first element
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone())
+        }
+    }
+
+    /// Return the head of a [`List`] which is the first element
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem )
+    }
+}
+
+
+///////////
+// Tests //
+///////////
+#[cfg(test)]
+mod tests {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let mut list = list.prepend(1).prepend(2).prepend(3);
+        let expected = vec![Some(&3), Some(&2), Some(&1), None];
+
+        for val in expected {
+            assert_eq!(list.head(), val);
+            list = list.tail();
+        }
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+    }
+}
